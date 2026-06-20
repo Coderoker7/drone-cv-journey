43fa0bc37a137853a4483c1ea4162683fefda0e3
Manav Dobriyal <manavdobriyal520@gmail.com>
2026-06-20 22:28:13 +0530

    Update second.py

---
+++ second.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+first = int(input('enter the first number:'))
+second = int(input('enter the second number:'))
+
+add = first+second
+print('the addition of 2 numbers are:', add)
