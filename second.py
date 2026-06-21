a8ed221d20b35f1f872e0db199d3307d6cb2dc34
Manav Dobriyal <manavdobriyal520@gmail.com>
2026-06-21 11:47:49 +0530

    updated second.py

--- second.py
+++ second.py
@@ -4,6 +4,7 @@ arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 first = int(input('enter the first number:'))
 second = int(input('enter the second number:'))
+third = int(input('enter the third number:'))
 
-add = first+second
-print('the addition of 2 numbers are:', add)
+add = first+second+third
+print('the addition of 3 numbers are:', add)
