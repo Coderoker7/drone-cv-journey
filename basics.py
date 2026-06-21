a5c471cb39467504fbe19485a307d24e892c31f8
Manav Dobriyal <manavdobriyal520@gmail.com>
2026-06-21 15:09:48 +0530

    basics.py

--- basics.py
+++ basics.py
@@ -2,7 +2,7 @@
 ## strings ##
 
 name = ' Manav Dobriyal '
-dob= 23_oct_2005
+dob = '23_twenty_three_oct'
 
 print(name)
 print(dob)
@@ -10,3 +10,8 @@ length = len(name)
 print(name[3])
 print(length)
 print(name[0:4])
+
+string_add = 'american \' potato'  # \ is an escape character
+# and \n is an escape sequence
+# after \ anything come its meaning change
+print(string_add)
