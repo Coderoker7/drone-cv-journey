44ff465a6f647ba6ae210dc028933c06e00e9088
Manav Dobriyal <manavdobriyal520@gmail.com>
2026-06-21 15:32:50 +0530

    new_module

--- basics.py
+++ basics.py
@@ -15,3 +15,15 @@ string_add = 'american \' potato'  # \ is an escape character
 # and \n is an escape sequence
 # after \ anything come its meaning change
 print(string_add)
+
+
+# formatted strings
+
+name = 'arnav'
+sirname = 'ghildiyal'
+format = f'my name is {len(name) } and sirname is {sirname}'
+print(format)
+# inside curly braces we can put any valid expression
+
+a = 1.1
+print(type(a))
