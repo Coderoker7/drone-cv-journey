b6bb0806fbb7be1e420408e31c64b4677e4db0f5
Manav Dobriyal <manavdobriyal520@gmail.com>
2026-06-26 15:43:18 +0530

    basics.problems

--- basics.py
+++ basics.py
@@ -1,155 +1,225 @@
-## learning basics of python ##
-## strings ##
+# ## learning basics of python ##
+# ## strings ##
 
-name = 'Manav Dobriyal'
-dob = '23_twenty_three_oct'
+# name = 'Manav Dobriyal'
+# dob = '23_twenty_three_oct'
 
-print(name)
-print(dob)
-length = len(name)
-print(name[3])  # letter find by index
-print(length)
-print(name[0:4:2])  # slicing => start:stop:step
-
-
-### ESCAPE SEQUENCES ###
-
-string_add = 'american \' potato'  # \ is an escape character
-# and \n is an escape sequence
-# after \ anything come its meaning change
-print(string_add)
+# print(name)
+# print(dob)
+# length = len(name)
+# print(name[3])  # letter find by index
+# print(length)
+# print(name[0:4:2])  # slicing => start:stop:step
 
 
-# formatted strings
+# ### ESCAPE SEQUENCES ###
 
-name = 'arnav'
-sirname = 'ghildiyal'
-format = f'my name is {len(name) } and sirname is {sirname}'
-print(format)
-# inside curly braces we can put any valid expression
-
-
-### TYPE OF DATA ###
-a = 1.1
-print(type(a))  # float
-print(type(range(4)))  # range(start, stop, step)
-print(type('hello'))  # str
-print(type([1, 2, 3, 4]))  # list
-# list,range,strings are iterable data types
+# string_add = 'american \' potato'  # \ is an escape character
+# # and \n is an escape sequence
+# # after \ anything come its meaning change
+# print(string_add)
 
 
-### comparison operators=> if-elif-else ###
-# and or not => logical operators
+# # formatted strings
 
-temp = 30
-if temp > 30 or temp == 30:
-    print('it is hot')
-elif temp < 30:
-    print('it is cold ')
-elif temp >= 30:
-    print('it is normal')
-
-person = True
-person1 = False
-if person:
-    print('true')
-if not person1:
-    print('false')
+# name = 'arnav'
+# sirname = 'ghildiyal'
+# format = f'my name is {len(name) } and sirname is {sirname}'
+# print(format)
+# # inside curly braces we can put any valid expression
 
 
-### Ternary operator ###
-age = 19
-message = 'eligible' if age > 18 else 'not eligible'
-print(message)
+# ### TYPE OF DATA ###
+# a = 1.1
+# print(type(a))  # float
+# print(type(range(4)))  # range(start, stop, step)
+# print(type('hello'))  # str
+# print(type([1, 2, 3, 4]))  # list
+# # list,range,strings are iterable data types
 
 
-### loops ###
+# ### comparison operators=> if-elif-else ###
+# # and or not => logical operators
 
-for no in range(1, 5):
-    print(no, no*'.')
+# temp = 30
+# if temp > 30 or temp == 30:
+#     print('it is hot')
+# elif temp < 30:
+#     print('it is cold ')
+# elif temp >= 30:
+#     print('it is normal')
 
-### for else loop ###
-
-sucess = False
-for no in range(1, 5):
-    print(no)
-    if sucess:
-        print('sucess')
-        break
-else:
-    print('not sucess')
-
-
-## Nested loops ##
-# firstly inner loop will completely execute then outer
-# loop will execute
-for i in range(3):
-    for j in range(3):
-        print(i, j)
+# person = True
+# person1 = False
+# if person:
+#     print('true')
+# if not person1:
+#     print('false')
 
 
-## while loop ##
-# we use while loop when we dont know how many times
-# we have to execute the loop
-
-condition = True
-while condition:
-    print('hello world')
-    condition = False
-
-### Another program ###
-print('to stop pls enter no')
-word = 'yes'
-while word != 'no':
-    word = input('enter a word: ')
-    print(f'{word}')
-    word = word.lower()
-
-    ### Functions ###
-
-# 1.) Function with no argument and no return keyword
+# ### Ternary operator ###
+# age = 19
+# message = 'eligible' if age > 18 else 'not eligible'
+# print(message)
 
 
-def greet():
-    print('hello world')
+# ### loops ###
+
+# for no in range(1, 5):
+#     print(no, no*'.')
+
+# ### for else loop ###
+
+# sucess = True
+# for no in range(1, 5):
+#     print(no)
+#     if sucess:
+#         print('sucess')
+#         break
+# else:
+#     print('not sucess')
 
 
-greet()
-
-# 2.) Function with argument and no return keyword
-
-
-def greet(name, sirname):  # parameters inside when creating a
-    # function
-    print(f'Hello {name} {sirname}')
+# ## Nested loops ##
+# # firstly inner loop will completely execute then outer
+# # loop will execute
+# for i in range(3):
+#     for j in range(3):
+#         print(i, j)
 
 
-greet('potato', 'dobriyal')  # Arguments when calling a function
+# ## while loop ##
+# # we use while loop when we dont know how many times
+# # we have to execute the loop
 
-# 3.) Function with arguments and return keyword
+# condition = True
+# while condition:
+#     print('hello world')
+#     condition = False
 
+# ### Another program ###
+# print('to stop pls enter no')
+# word = 'yes'
+# while word != 'no':
+#     word = input('enter a word: ')
+#     print(f'{word}')
+#     word = word.lower()
 
-def add(a, b):
-    return a+b
+#     ### Functions ###
 
-
-result = add(2, 3)  # we can store the return value and
-# use it anywhere in the code
-print(result)
-# by default every function return none if we dont use return
-# keyword
-
-## Augmented assignment operator ##
-a = 5
-a += 2  # a=a+2
-print(a)
-
-## *xargs and **kwargs ##
-
-
-def func(*args, **kwargs):
-    print(args)  # tuple of positional arguments
-    print(kwargs)  # dictionary of keyword arguments
+# # 1.) Function with no argument and no return keyword
 
 
-func(1, 2, 3, 4, 5, name='johny', sirname='depp')
+# def greet():
+#     print('hello world')
+
+
+# greet()
+
+# # 2.) Function with argument and no return keyword
+
+
+# def greet(name, sirname):  # parameters inside when creating a
+#     # function
+#     print(f'Hello {name} {sirname}')
+
+
+# greet('potato', 'dobriyal')  # Arguments when calling a function
+
+# # 3.) Function with arguments and return keyword
+
+
+# def add(a, b):
+#     return a+b
+
+
+# result = add(2, 3)  # we can store the return value and
+# # use it anywhere in the code
+# print(result)
+# # by default every function return none if we dont use return
+# # keyword
+
+# ## Augmented assignment operator ##
+# a = 5
+# a += 2  # a=a+2
+# print(a)
+
+# ## *xargs and **kwargs ##
+
+
+# def func(*args, **kwargs):
+#     print(args)  # tuple of positional arguments
+#     print(kwargs)  # dictionary of keyword arguments
+
+
+# func(1, 2, 3, 4, 5, name='johny', sirname='depp')
+
+
+# ## Tuples and lists ##
+# # tuples are immutable and lists are mutable
+# tuple = (1, 2, 3, 4, 5)
+# tuple[0] = 10  # this will give error cause tuples are immutable
+# print(tuple)
+
+# list = [1, 2, 3, 4, 5]
+# list[0] = 10  # this will not giver error cause lists are mutable
+# print(list)
+
+# ## DICTIONARY ##
+# # Dictionaries are mutable and unordered data types
+# dict = {'name': 'manav', 'sirname': 'dobriyal', 'age': 23}
+# print(dict['name'])  # access value by key
+# print(dict.get('name'))  # access value by key (safer way)
+# # it will not give error if key is
+# # not present
+# dict['learning'] = 'computer vision'
+# print(dict[0])  # this will give error casue there is no order/index
+
+## SETS ##
+
+# sets are unordered and immutable data type
+# we can't store dictionary and list in sets cause they are
+# mutable we can only store immutalbe data type
+# set contain unique element and if any element repeat
+# the repeating element automatically removed by set
+
+# print(type({}))  # dict
+# print(type([]))  # list
+# print(type((1,)))  # tuple
+# print(type(set())) # set
+
+
+################## CLASSES AND OBJECT ####################
+
+# CLASSES are the blueprint for object
+# => cause it define what an object should contain
+
+class student:
+    colledge_name = 'DOBRIYAL COLLEDGE'  # class attribute
+
+    def __init__(self, name):  # constructor
+        self.name = name
+
+    def greet_fun(self):  # method for objects/instance
+        print('Hello welcome to dobriyal colledge\n', self.name)
+
+
+stud_1 = student('manav')  # object attributes
+stud_2 = student('kunal')
+print(student.colledge_name)
+stud_1.greet_fun()
+
+
+#### Membership operators ( in , not in )####
+# They check whether something exists
+# inside a string, list, tuple, set, etc.
+# return True, False (boolean value)
+
+
+### enumerate() ###
+# it is a function used to get both index and value present
+# at that index.
+
+list = ['manav', 'arnav', 'kunal']
+for i, value in enumerate(list):
+    print(i+1, value)
--- basics_problems.py
+++ basics_problems.py
@@ -1,83 +1,205 @@
-### performing basic arithmetic operations ###
-a = 3
-b = 2
-print(f'sum of two no\'s are {a+b}')
-print(f'sub of two no are {a-b}')
-print(f'product of two no are {a*b}')
+# ### performing basic arithmetic operations ###
+# a = 3
+# b = 2
+# print(f'sum of two no\'s are {a+b}')
+# print(f'sub of two no are {a-b}')
+# print(f'product of two no are {a*b}')
 
 
-### performing basic comparison operators ###
-n = int(input('enter no:'))
+# ### performing basic comparison operators ###
+# n = int(input('enter no:'))
 
-if n % 2 != 0 or (n % 2 == 0 and 6 <= n <= 20):
-    print('Weird')
-elif (n % 2 == 0 and n > 20) or (n % 2 == 0 and 2 <= n <= 5):
-    print('Not Weird')
+# if n % 2 != 0 or (n % 2 == 0 and 6 <= n <= 20):
+#     print('Weird')
+# elif (n % 2 == 0 and n > 20) or (n % 2 == 0 and 2 <= n <= 5):
+#     print('Not Weird')
 
- # task :
- # The provided code stub reads an integer,n , from STDIN.
- #  For all non-negative integers i<n , print i^2 .
+#  # task :
+#  # The provided code stub reads an integer,n , from STDIN.
+#  #  For all non-negative integers i<n , print i^2 .
 
-no = int(input('enter no:'))
+# no = int(input('enter no:'))
 
-for no in range(no, 0, -1):
-    print(no*no)
-
-
-## Leap year code ##
-
-def is_leap_year(year):
-    if year % 400 == 0:
-        return True
-    elif year % 4 == 0 and year % 100 != 0:
-        return True
-    else:
-        return False
+# for no in range(no, 0, -1):
+#     print(no*no)
 
 
-year = int(input('enter year:'))
+# ## Leap year code ##
 
-if is_leap_year(year):
-    print('leap year')
-else:
-    print('not a leap year')
-
-
-# task:Given the participants' score sheet for your University
-# Sports Day, you are required to find the runner-up score.
-# You are given  scores.
-# Store them in a list and find the score of the runner-up.
-list = [1, 2, 9, 10, 8, 3]
-
-sorted = list.sort(reverse=True)  # list returns none
-i = 0
-for no in list:
-    if i == 1:
-        print(f'{no} is runner up score')
-    else:
-        pass
-    i += 1
-
-# checking a no is prime or not
+# def is_leap_year(year):
+#     if year % 400 == 0:
+#         return True
+#     elif year % 4 == 0 and year % 100 != 0:
+#         return True
+#     else:
+#         return False
 
 
-def is_prime_no(num):
-    if num <= 1:
-        return False
-    else:
-        for no in range(2, int(num**0.5)+1):
-            if num % no == 0:
+# year = int(input('enter year:'))
 
-                return False
-
-    return True
+# if is_leap_year(year):
+#     print('leap year')
+# else:
+#     print('not a leap year')
 
 
-integer = int(input('Enter any no:'))
+# # task:Given the participants' score sheet for your University
+# # Sports Day, you are required to find the runner-up score.
+# # You are given  scores.
+# # Store them in a list and find the score of the runner-up.
+# list = [1, 2, 9, 10, 8, 3]
 
-accept = is_prime_no(integer)
+# sorted = list.sort(reverse=True)  # list returns none
+# i = 0
+# for no in list:
+#     if i == 1:
+#         print(f'{no} is runner up score')
+#     else:
+#         pass
+#     i += 1
 
-if not accept:
-    print('it is not a prime no')
-else:
-    print('it is a prime no')
+# # checking a no is prime or not
+
+
+# def is_prime_no(num):
+#     if num <= 1:
+#         return False
+#     else:
+#         for no in range(2, int(num**0.5)+1):
+#             if num % no == 0:
+
+#                 return False
+
+#     return True
+
+
+# integer = int(input('Enter any no:'))
+
+# accept = is_prime_no(integer)
+
+# if not accept:
+#     print('it is not a prime no')
+# else:
+#     print('it is a prime no')
+
+# class student:
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+
+#     def fun(self):
+#         return (f'''Hello, my name is {self.name} and marks
+#                 are {self.marks[0]},
+#                 {self.marks[1]},{self.marks[2]}''')
+
+
+# stu1 = student('manav', [88, 99, 100])
+# stu2 = student('kunal', [88, 99, 100])
+# print(stu1.fun())
+
+# Task:Both players are given the same string, .
+# Both players have to make substrings using the
+# letters of the string .
+# Stuart has to make words starting with consonants.
+# Kevin has to make words starting with vowels.
+# The game ends when both players have made all
+# possible substrings.
+# Scoring:
+# A player gets +1 point for each occurrence of the
+# substring in the string .
+# def minion_game(word):
+#     kevin = 0
+#     stuart = 0
+#     vowels = "AEIOU"
+
+#     for i, ch in enumerate(word):
+#         points = len(word) - i
+
+#         if ch in vowels:
+#             kevin += points
+#         else:
+#             stuart += points
+
+#     if kevin > stuart:
+#         print("Kevin", kevin)
+#     elif stuart > kevin:
+#         print("Stuart", stuart)
+#     else:
+#         print("Draw")
+
+
+# word = input("Enter a word: ").upper()
+# minion_game(word)
+
+
+# swap_case #
+
+
+# def swap_fun(character):
+#     strin = ''
+#     for i, ch in enumerate(character):
+#         if ch.islower():
+#             convert_upper = ch.upper()
+#             strin += convert_upper
+#         else:
+#             convert_lower = ch.lower()
+#             strin += convert_lower
+#     return strin
+
+
+# inp = input('enter any string:')
+# new_str = swap_fun(inp)
+# print(new_str)
+
+# list problem #
+# common operations on list
+# def list_cmd(n):
+#     list = []
+#     print(f'{n} no of commands will be performed')
+#     list.insert(0, 5)
+#     list.insert(1, 6)
+#     list.insert(2, 8)
+#     list.insert(3, 9)
+#     print(list)
+#     list.remove(5)
+#     list.append(7)
+#     list.sort()
+#     list.pop()
+#     list.reverse()
+#     print(list)
+
+
+# inp = 11
+# list_cmd(inp)
+
+## nested list ##
+
+lis = [['manav', 99], ['rahul', 22], ['karan', 33], ['arnav', 22],
+       ['shelly', 33]]
+sorted_list = []
+name_list = []
+count = 0
+second_lowest_marks = []
+
+for i in lis:
+    sorted_list.append(i[1])  # here we are appending marks of
+    # all students in a list.
+    name_list.append(i[0])  # here we are appending students name
+sorted_list.sort()
+
+sorted_list = list(set(sorted_list))
+sorted_list.sort()
+
+# sorting marks ascending order
+print(sorted_list)
+
+for i in sorted_list:
+    if count == 1:
+        second_lowest_marks = sorted_list[1]  # here we are assigning
+        # second lowest grade to new list
+    count += 1
+
+for name in lis:
+    if second_lowest_marks == name[1]:
+        print(f'''the person who has lowest score is {name[0]}
+               which is {name[1]}''')
